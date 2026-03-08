73a3f63f1c9a513d7d2a21cdb51f46460bb01cf7
Hebown <151939174+Hebown@users.noreply.github.com>
2026-03-08 16:35:25 +0800

    完成了Scalar反色

--- labs/lab1/task-list.cpp
+++ labs/lab1/task-list.cpp
@@ -16,23 +16,10 @@ int main(){
     std::cout << "image rows (height) = " << image.rows << std::endl; // 图像的高度
     std::cout << "image cols (width) = " << image.cols << std::endl; // 图像的宽度
     
-    // 遍历每个像素
-    for (int i = 0; i < image.rows; i++) {
-        for (int j = 0; j < image.cols; j++) {
-            // 获取当前像素
-            cv::Vec3b pixel = image.at<cv::Vec3b>(i, j);
-            
-            // 判断是否为白色（BGR三个通道都为255）
-            // 白色在BGR中为 (255, 255, 255)
-            if (pixel[0] == 255 && pixel[1] == 255 && pixel[2] == 255) {
-                // 将白色修改为黑色 (0, 0, 0)
-                image.at<cv::Vec3b>(i, j) = cv::Vec3b(0, 0, 0);
-            }
-        }
-    }
+    // 反色，一种方法是 使用 Scalar
+    cv::Mat result = cv::Scalar(255, 255, 255) - image;
     
-    // 保存修改后的图像
-    cv::imwrite("opencv-logo-modified.png", image);
+    cv::imwrite("opencv-logo-inverted-scalar.png", result);
 
     cv::waitKey(0); // 等待按键
     return 0;
